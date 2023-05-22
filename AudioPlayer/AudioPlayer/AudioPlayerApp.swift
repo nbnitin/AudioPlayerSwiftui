f18b399e136362446adfb4a3848a3d0e1bfebab7
Nitin <nbnitin.bhatia@gmail.com>
2023-05-22 20:14:42 +0530

    project added...

---
+++ AudioPlayer/AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Nitin Bhatia on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
